2cae6ad22c9327507769f2b39f93008538c413bc
Miki Namsrai <miki.namsrai@gmail.com>
2020-11-02 21:16:43 +0100

    finish profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -8,10 +8,13 @@
       I'm a web and mobile app developer base in Lyon, France.
     </div>
     <div class="text-white font-normal text-6xl">
-      Through the past years, I've been working on projects of different kind. From the FullStack Web Apps to mobile ones using Flutter.
+      Through the past years, I've been working on projects of different kind. From the FullStack Web Apps to mobile ones using <span class="text-blue-400">Flutter</span>.
     </div>
-    <div class="flex flex-row mt-12 text-white text-xl leading-8">
-      <span class="click-see-profile cursor-pointer hover:text-purple-700 transition duration-500">Back to Home Page</span>
+    <div class="text-white font-normal text-6xl mt-12">
+      Building websites, games, phone apps has been a huge part of my life, that's why I dedicate myself entirely to the universe of content development. This hobby started by a simple Pong game coded with <span class="text-red-700">Java</span>.
+    </div>
+    <div class="text-white font-normal text-6xl mt-12">
+      Nowadays, I use tools to create web apps such as <span class="text-blue-700">React</span> (Next.js) or <span class="text-green-700">Vue</span> (Nuxt)  
     </div>
     <Footer />
   </div>
